0c0430f438d2a20ecbf7476392fb7b3ef1aa862c
tamar96033 <tamar96033@gmail.com>
2025-02-10 19:01:47 +0200

    5

--- TodoApi/Item.cs
+++ TodoApi/Item.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TodoApi;
 
+[Table("Items")]
 public partial class Item
 {
     public int Id { get; set; }
