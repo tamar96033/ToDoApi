8a0babc82069c1af6bb420d92bbb56453887bc09
tamar96033 <tamar96033@gmail.com>
2025-02-10 19:42:36 +0200

    r2

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = "${process.env.REACT_APP_API_URL}/items"
+const apiUrl = `${process.env.REACT_APP_API_URL}/items`
 
 export default {
   getTasks: async () => {
